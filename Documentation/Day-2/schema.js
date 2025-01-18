621d48d467056643dbb196f36d93a0641edbff29
kinza003 <kinzanaseer321321@gmail.com>
2025-01-18 20:12:30 +0500

    Marketplace Day-2

---
+++ Documentation/Day-2/schema.js
@@ -0,0 +1,33 @@
+export default {
+    name: 'product',
+    title: 'Product',
+    type: 'document',
+    fields: [
+      {
+        name: 'id',
+        title: 'ID',
+        type: 'string',
+      },
+      {
+        name: 'name',
+        title: 'Name',
+        type: 'string',
+      },
+      {
+        name: 'image',
+        title: 'Image',
+        type: 'image',
+      },
+      {
+        name: 'price',
+        title: 'Price',
+        type: 'number',
+      },
+      {
+        name: 'description',
+        title: 'Description',
+        type: 'string',
+      },
+    ],
+  };
+  
